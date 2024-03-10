6db81ab2407c0950ab6ab9a02652a94717beaf05
Vítor Lobo <vitorlobof@gmail.com>
2024-03-10 05:26:22 -0300

    refactor: The button now works by interruption.

--- include/main.h
+++ include/main.h
@@ -4,7 +4,7 @@
 
 #define SERVO_PIN 4
 #define SENSOR_PIN A0
-#define BUTTON_PIN 5
+#define BUTTON_PIN 21
 
 CServo servo;
 Sensor sensor;
@@ -13,3 +13,19 @@ int state = LOW; // A chave começa fechada.
 
 float currentLimit = 10.0; // Limite de corrente aceito. Ao ser superado, a chave será aberta.
 float current;             // Vai guardar as leituras de corrente.
+
+void swapState()
+{
+    state = !state;
+
+    if (state == HIGH)
+    {
+        servo.toAngle(90); // Abre a chave.
+    }
+    else
+    {
+        servo.toAngle(0); // Fecha a chave.
+    }
+
+    Serial.println("O botão foi pressionado");
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,13 @@ void setup()
   sensor.attach(SENSOR_PIN);
 
   servo.attach(SERVO_PIN);
-  servo.write(0); // O servo começa com ângulo 0.
+  servo.write(0); // O servo começa com ângulo 0. Ou seja, chave fechada.
 
   pinMode(BUTTON_PIN, INPUT);
+  attachInterrupt(
+      digitalPinToInterrupt(BUTTON_PIN),
+      swapState,
+      CHANGE);
 }
 
 void loop()
@@ -19,26 +23,10 @@ void loop()
   if (abs(current) > currentLimit)
   {
     state = HIGH;
-    servo.toAngle(90);
+    servo.toAngle(90); // Abre a chave.
 
     Serial.print("Sobrecorrente: ");
     Serial.print(abs(current));
-    Serial.println(" A\n");
-  }
-
-  if (digitalRead(BUTTON_PIN) == HIGH)
-  {
-    state = !state;
-
-    if (state == HIGH)
-    {
-      servo.toAngle(90);
-    }
-    else
-    {
-      servo.toAngle(0);
-    }
-
-    Serial.println("O botão foi precionado\n");
+    Serial.println(" A");
   }
 }
